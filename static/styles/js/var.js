14d20fc1e1196daa6c0c20bbfce41544100931a7
Maël Valais <mael.valais@gmail.com>
2017-11-22 09:50:25 +0100

    removed apparently duplicate

--- static/styles/js/var.js
+++
@@ -1,8 +0,0 @@
-
-var creator 			= "touist";
-var project 			= "touist";
-var branch  			= "master";
-var changelogfile 		= "CHANGELOG";
-var licencefile 		= "LICENSE";
-var participatefile		= "CONTRIBUTING.md";
-var contributorfile 	= "CONTRIBUTORS.md"
